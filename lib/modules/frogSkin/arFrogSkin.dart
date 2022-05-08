9e16a5a1fdcb45a3d686602ca4c2ba99b5f2142c
Rowan Mahmoud <you@example.com>
2022-05-08 14:36:25 +0200

    first commit

---
+++ lib/modules/frogSkin/arFrogSkin.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ARFrogSkin extends StatefulWidget {
+  @override
+  _HelloWorldState createState() => _HelloWorldState();
+}
+
+class _HelloWorldState extends State<ARFrogSkin> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      );
+
+  }
+
+
+
+
+
+
+
+}
+
